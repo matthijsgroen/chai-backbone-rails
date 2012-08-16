166cbcc21b770b565ccbff078a80af9980760466
Matthijs Groen <matthijs.groen@gmail.com>
2012-08-16 22:30:23 +0200

    init

---
+++ lib/chai-backbone-rails.rb
@@ -0,0 +1,9 @@
+require "chai-backbone-rails/version"
+
+module Chai
+  module Backbone
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/chai-backbone-rails/version.rb
@@ -0,0 +1,7 @@
+module Chai
+  module Backbone
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
